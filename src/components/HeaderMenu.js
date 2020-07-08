0f9e2faa861a65cc226a107e765f9f0724ee0574
lyc2760008 <44538498+lyc2760008@users.noreply.github.com>
2020-07-07 23:13:07 -0600

    make it better

--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -3,26 +3,9 @@ import './MovieDetails.css'
 import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
-import { NavLink } from 'react-router-dom';
-import {Route,Redirect} from 'react-router-dom';
+
 
 class HeaderMenu extends React.Component{
-  async componentDidMount() {
-    //Fetch data it has not been stored in local storage
-    try {
-    const url = "https://ancient-reef-50076.herokuapp.com/api/getUser";
-      const response = await fetch(url);
-      const jsonData=await response.json()
-      this.setState( {jsonData: jsonData } );
-      //this.state.jsonData.sort((a, b) => (a.title > b.title) ? 1 : -1)
-      //this.setState( {movies: this.state.jsonData } );
-      }
-      catch (error) {
-        console.error(error);
-      }
-
- }
-
 
   //handle show or hide the About component
     state={
@@ -49,11 +32,6 @@ class HeaderMenu extends React.Component{
         this.setState({showPro: true})
     }
 
-    // handleLogOut =() => {
-    //       fetch('api/users')
-    //     .then(response => response.json())
-    //     .then(function (data){console.log(data)})
-    // }
 
     render(){
         return (
@@ -62,20 +40,13 @@ class HeaderMenu extends React.Component{
             <Button variant="primary" onClick={this.handleShow}>
               About
             </Button>
-            {/* <Button variant="primary" onClick={this.handleProShow}>
-              Profile
-            </Button> */}
-            {/* <Button variant="primary"><NavLink exact to='/profile' activeClassName='active'>Profile
-            </NavLink></Button> */}
-          {/* <Button variant="primary"><a href={'https://ancient-reef-50076.herokuapp.com/logout'}><span className='logout'>Log out</span></a>
-            </Button> */}
             <Modal show={this.state.show} onHide={this.handleClose}>
               <Modal.Header closeButton>
-                <Modal.Title>University Project</Modal.Title>
+                <Modal.Title>University React Project</Modal.Title>
               </Modal.Header>
               <Modal.Body>
                 <p>Author: Yichen Li</p>
-                <p><a href='https://lyc2760008.github.io/test'>Github Link</a></p>
+                <p><a href='https://lyc2760008.github.io/test'>Github Deployment Link</a></p>
                 <p>Technology used: React</p>
                 <p>Learning sources:</p>
                 <ul className="fa-ul"> 
@@ -91,38 +62,6 @@ class HeaderMenu extends React.Component{
               </Modal.Body>
               <Modal.Footer>
                 <Button variant="secondary" onClick={this.handleClose}>
-                  Close
-                </Button>
-              </Modal.Footer>
-            </Modal>
-
-
-            <Modal show={this.state.showPro} onHide={this.handleProClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>User Information</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-              <img src = {this.state.jsonData && this.state.jsonData.user && this.state.jsonData.user.picture && this.state.jsonData.user.picture.thumbnail}></img>
-                <p> 
-                  First Name: {this.state.jsonData && this.state.jsonData.user && this.state.jsonData.user.details &&
-                                  this.state.jsonData.user.details.firstname}
-                </p>
-                <p> 
-                  Last Name: {this.state.jsonData && this.state.jsonData.user && this.state.jsonData.user.details &&
-                                  this.state.jsonData.user.details.lastname}
-                </p>
-                <p> 
-                  City: {this.state.jsonData && this.state.jsonData.user && this.state.jsonData.user.details &&
-                                  this.state.jsonData.user.details.city}
-                </p>
-                <p> 
-                  Country: {this.state.jsonData && this.state.jsonData.user && this.state.jsonData.user.details &&
-                                  this.state.jsonData.user.details.country}
-                </p>
-
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={this.handleProClose}>
                   Close
                 </Button>
               </Modal.Footer>
